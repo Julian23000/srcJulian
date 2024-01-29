9ae92d9c60edbf9b70411904780ccbfdd1847086
Julian23000 <143167143+Julian23000@users.noreply.github.com>
2024-01-29 15:57:29 +0200

    Ex2 and webpage

--- JH_tasks/ex1.php
+++ JH_tasks/ex1.php
@@ -1,11 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>DocumExercise 1: Getting Started with PHP - Julian</title>
-</head>
-<body>
+    <?php 
+    $title = "Exercise 1";
+    include 'header.php'; ?>
+
+
     <h3>3.1 Write PHP code to output the following message:
     Hello world! My name is "David"
     </h3>
@@ -58,5 +55,6 @@
         and include it as an image in the "ex1.php" file.</h3>
 
     <img src="images/Ex1Pic.png">
-</body>
-</html>
+
+
+    <?php include 'footer.php'; ?>
---
+++ JH_tasks/ex2.php
@@ -0,0 +1,12 @@
+<?php 
+$title = "Exercise 2";
+include 'header.php'; ?>
+
+
+<h3> dslajfdsklj</h3>
+
+
+
+
+
+<?php include 'footer.php'; ?>
