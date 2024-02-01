1cdb6c3f8c5216b96212c9da827032106ecb8cd6
Julian23000 <143167143+Julian23000@users.noreply.github.com>
2024-02-01 11:50:44 +0200

    s

--- JH_tasks/process1.php
+++ JH_tasks/process1.php
@@ -3,3 +3,8 @@
 
 $nimi = $_POST["nimi"];
 $email = $_POST["email"]:
+
+$sukunimi= $_POST["sukunimi"];
+$etunimi = $_POST["etunimi"];
+
+echo "Hello " . $sukunimi . $etunimi . ", Welcome to the webpage"
