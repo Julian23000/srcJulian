dbf146ce1c092c8aa74d8c6757f8373f1745c0ff
Julian23000 <143167143+Julian23000@users.noreply.github.com>
2024-02-01 10:59:05 +0200

    update

---
+++ JH_tasks/ex3.php
@@ -0,0 +1,16 @@
+<?php
+$title = "Ex3";
+include 'header.php'; ?>
+
+
+<h1>Form Creation: Create a simple HTML form to collect the user's Firstname and Lastname. Use the echo statement to print “Hello [Firstname] [Lastname], You are welcome to my site.” inside an h3 tag </h1>
+
+<form method="post" action=> 
+    Name: <input type="text" name="nimi" required> <br></br>
+    Email: <input type="email" name="email" required> <br></br>
+    DOB: <imput type="date" name="dob"> <br></br>
+    Select Color: <input type="color" name="color"> <br></br>
+    <input type="submit" name="submit"> <br></br>
+
+
+<?php include 'footer.php'; ?>
---
+++ JH_tasks/process1.php
@@ -0,0 +1,5 @@
+<?php
+//what to do with the form data
+
+$nimi = $_POST["nimi"];
+$email = $_POST["email"]:
