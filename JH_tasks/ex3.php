5fbfcd1c2aa4a36698776068d1e0b2ecb1a9855d
Julian23000 <143167143+Julian23000@users.noreply.github.com>
2024-02-03 17:35:44 +0200

    tasks

--- JH_tasks/ex3.php
+++ JH_tasks/ex3.php
@@ -12,5 +12,46 @@ include 'header.php'; ?>
     Select Color: <input type="color" name="color"> <br></br>
     <input type="submit" name="submit"> <br></br>
 
+<h2>HTML Table: Prepare a simple HTML table and apply Bootstrap styles to it. Note, you have already done a table in Exercise</h2>
+
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Julian</td>
+      <td>Hack</td>
+      <td>@BD</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Liisi</td>
+      <td>Kuusk</td>
+      <td>@Instagram</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@KFC</td>
+    </tr>
+  </tbody>
+</table>
+
+<h3> Write a PHP script with two string variables ($str1 = "Hello"; $str2 = "World";). Join them together and print the length of the string.</h3>
+
+<?php
+
+$str1 = "Hello ";
+$str2 = "fellow students";
+
+echo $str1 . $str2;
+?>
 
 <?php include 'footer.php'; ?>
