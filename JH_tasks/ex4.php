ebc4948d80bd316936c141c86bf19b897a785a24
Julian23000 <143167143+Julian23000@users.noreply.github.com>
2024-02-04 19:30:29 +0200

    ex4

--- JH_tasks/ex4.php
+++ JH_tasks/ex4.php
@@ -2,5 +2,21 @@
 $title = "Exercise 4";
 include 'header.php'; ?>
 
+<h1>If-Else: Write a PHP script to get inputs (age and name) from the user and based on their age, decide if he/she is eligible for voting. (18 or more than 18 years is eligible for voting, use form to get user input).</h1>
+
+<?php 
+    $Name;
+    $Age;
+
+    if($Age >= 18)
+    {
+        echo "The person is eligible for voting!";
+    }
+
+    else
+    {
+        echo "The person is not eligible for voting!";
+    }
+?>
 
 <?php include 'footer.php'; ?>
