52d9e2adea8a9e521a50e743c7a436d54da7f9f3
Julian23000 <143167143+Julian23000@users.noreply.github.com>
2024-02-07 15:35:21 +0200

    db

--- JH_tasks/crud1/db.php
+++ JH_tasks/crud1/db.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "php24-db-1"; // Replace with your MySQL server hostname
-$username = "app1";     // Replace with your MySQL username
-$password = "LiisiKuusk6";     // Replace with your MySQL password
-$dbname = "app1";       // Replace with the name of your MySQL database
+$servername = "localhost"; // Replace with your MySQL server hostname
+$username = "julian23000";     // Replace with your MySQL username
+$password = "8IiuTJrM";     // Replace with your MySQL password
+$dbname = "wp_julian23000";       // Replace with the name of your MySQL database
 
 // Create a database connection
 $conn = new mysqli($servername, $username, $password, $dbname);
