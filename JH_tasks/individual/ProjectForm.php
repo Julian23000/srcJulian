9b792fc424afd6fc8aaae456879202d666990cb4
Julian23000 <143167143+Julian23000@users.noreply.github.com>
2024-02-12 15:52:10 +0200

    s

---
+++ JH_tasks/individual/ProjectForm.php
@@ -0,0 +1,19 @@
+<?php 
+$title = "Picture Submission";
+include 'header.php'; ?>
+
+
+
+
+
+Name: <input type="text" name="name" value="<?php echo $name;?>">
+
+E-mail: <input type="text" name="email" value="<?php echo $email;?>">
+
+Website: <input type="text" name="website" value="<?php echo $website;?>">
+
+Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
+
+
+
+<?php include 'footer.php'; ?>
