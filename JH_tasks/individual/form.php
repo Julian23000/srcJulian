851bc5ba898f598bb02ebc6d9888159faeb42309
Julian23000 <143167143+Julian23000@users.noreply.github.com>
2024-02-12 10:37:23 +0200

    form

--- JH_tasks/individual/form.php
+++ JH_tasks/individual/form.php
@@ -4,12 +4,74 @@ include 'header.php';
 ?>
 
 <form action="" method="post">
-Name: <input type="text" name="fname" required minlength="3" maxlength="30">
-Email: <input type="email" name="email" required >
-Password: <input type="password" name="password" required minlength="7" maxlength="20">
-Age: <input type="number" name="age" min="18" max="100" >
+Name: <input type="text" name="fname" required minlength="3" maxlength="30" id="name"> <br>
+<span id ="nameError"></span><br>
+Email: <input type="email" name="email" required id="email"> <br>
+<span id ="emailError"></span><br>
+Password: <input type="password" name="password" required minlength="7" maxlength="20" id="password"> <br>
+<span id ="passwordError"></span><br>
+Age: <input type="number" name="age" min="18" max="100" id="age"> <br>
+<span id ="ageError"></span><br>
 <button type="submit" name="regSub"> Submit</button>
 </form>
+
+<script>
+function validateName() {
+    const name = document.getElementById("name").value;
+    const nameError = document.getElementById("nameError");
+
+    if (name.length < 3 || name.length > 20) {
+        nameError.innerHTML = "Name must be between 3 & 20 characters";
+        return false;
+    } else {
+        nameError.innerHTML = "";
+        return true;
+    }
+}
+
+function validateEmail() {
+    const email = document.getElementById("email").value;
+    const emailError = document.getElementById("emailError");
+
+    if (email === "" || !email.includes("@")) {
+        emailError.innerHTML = "Please enter a valid email address";
+        return false;
+    } else {
+        emailError.innerHTML = "";
+        return true;
+    }
+}
+
+function validatePassword() {
+    const password = document.getElementById("password").value;
+    const passwordError = document.getElementById("passwordError");
+
+    if (password.length < 6) {
+        passwordError.innerHTML = "Your password must be at least 6 characters long";
+        return false;
+    } else {
+        passwordError.innerHTML = "";
+        return true;
+    }
+}
+
+function validateAge() {
+    const age = document.getElementById("age").value;
+    
+}
+
+// event listeners for real time validation
+document.getElementById("name").addEventListener("input", validateName);
+document.getElementById("email").addEventListener("input", validateEmail);
+document.getElementById("password").addEventListener("input", validatePassword);
+
+
+
+</script>
+
+
+
+
 
 
 <?php
