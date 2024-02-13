e2efe125a9b38eb496b49a0b60b27b094c71ef9a
Julian23000 <143167143+Julian23000@users.noreply.github.com>
2024-02-13 15:22:03 +0200

    update

---
+++ JH_tasks/Prject/upload.php
@@ -0,0 +1,14 @@
+<?php
+
+if(isset($_FILES['imageToUpload'])){
+  move_uploaded_file($_FILES['imageToUpload']['tmp_name'], "images/". $_FILES['imageToUpload']['name']);
+}
+else{
+    echo "image not found!";
+}
+ 
+?>
+
+<?php
+move_uploaded_file(tmp_name, filepath.withName);
+?>
