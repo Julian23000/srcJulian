e8a290feab3181da5082041e8f9e081db13d9121
Julian23000 <143167143+Julian23000@users.noreply.github.com>
2024-02-13 15:46:39 +0200

    update

--- JH_tasks/Prject/upload.php
+++ JH_tasks/Prject/upload.php
@@ -1,14 +1,10 @@
 <?php
 
-if(isset($_FILES['imageToUpload'])){
-  move_uploaded_file($_FILES['imageToUpload']['tmp_name'], "images/". $_FILES['imageToUpload']['name']);
+if(isset($_FILES['fileToUpload'])){
+  move_uploaded_file($_FILES['fileToUpload']['tmp_name'], "Images/". $_FILES['fileToUpload']['name']);
 }
 else{
     echo "image not found!";
 }
  
-?>
-
-<?php
-move_uploaded_file(tmp_name, filepath.withName);
 ?>
